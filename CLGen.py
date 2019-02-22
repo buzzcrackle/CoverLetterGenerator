79bec9078e5999063190b4a001b1f82e143bba3c
Jesse Liang <jesseliang@ymail.com>
2019-02-22 09:32:44 -0800

    yes

--- CLGen.py
+++ CLGen.py
@@ -1,15 +1,25 @@
+from docx import Document
 import pyperclip
 import sys
 
-error = "Um... that's wrong. Something like:\npython CLGen.py Dropbox Intern"
+doc = Document('./CoverLetterTemplate.docx')
 
-f = open("coverletter.txt", "r")
-body = f.read()
+if len(sys.argv) == 4:
+    entireBody = ""
+    for p in doc.paragraphs:
+        if 'COMPANY_NAME' in p.text:
+            inline = p.runs
+            for i in range(len(inline)):
+                if 'COMPANY_NAME' in inline[i].text:
+                    text = inline[i].text.replace('COMPANY_NAME', sys.argv[1])
+                    inline[i].text = text
+                if 'JOB_TITLE' in inline[i].text:
+                    text = inline[i].text.replace('JOB_TITLE', sys.argv[2])
+                    inline[i].text = text
+        entireBody = entireBody + "\n" +  p.text
 
-if len(sys.argv) == 3:
-    body = body.replace("COMPANY_NAME", str(sys.argv[1]))
-    body = body.replace("JOB_TITLE", str(sys.argv[2]))
-    pyperclip.copy(body)
-    print(body)
+    pyperclip.copy(entireBody)
+    print(entireBody)
+    doc.save('./' + sys.argv[3] + '.docx')
 else:
-    print(error)
+    print("3 arguments required, company name, job, your name")
--- README.md
+++ README.md
@@ -3,11 +3,14 @@ Python script to generate cover letters. Built on Python version 2.7.15
 
 ## How to use:
 
-1. In the same folder/directory as the python file, place a txt file with your cover letter template, named "coverletter.txt".
+1. In the same folder/directory as the python file, place a docx file with your cover letter template, named "CoverLetterTemplate.docx".
 
 2. Replace the all occurances of the name of the company with the following text, "COMPANY_NAME". Then, replace all occurances of the job title with the following text, "JOB_TITLE".
 
 3. Finally, you can run the script with the following command:
-	python CLGen.py "INSERT COMPANY NAME" "INSERT JOB TITLE"
+	python CLGen.py "INSERT COMPANY NAME" "INSERT JOB TITLE" "INSERT FILE NALE"
+    Note: the file name DOES NOT include the .docx extension.
+
+    Ex: python CLGen.py "Google" "Software Developer" "John_Doe_Cover_Letter"
 
 4. The cover letter text will be automatically copied onto your clipboard, but it will also be printed out onto the terminal.
